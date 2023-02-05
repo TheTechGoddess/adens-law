c2b9b4f6170baf0e6e628f951acf3dbcc1d2852a
Favour-Cell <favourenwonwukc@gmail.com>
2023-02-05 11:17:22 +0100

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-black w-full flex justify-between px-20 py-5">
+    <div class="flex">
+      <img src="~/assets/images/logo.png" class="mr-2 -mt-0.5"/>
+      <p class="mt-1 text-[#616161]"> <span  class="text-white">Adens Law</span> Group</p>
+    </div>
+      <ul class="hidden md:flex justify-between mt-1 w-[30%]">
+        <li  class="text-white">Home</li>
+        <li  class="text-white">Practice areas</li>
+        <li  class="text-white">The team</li>
+        <li  class="text-white">Blog</li>
+      </ul>
+    <div class="flex">
+      <button class="text-white border border-white px-3 py-0.5 rounded-lg">Get in touch</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
